17373d4865989a64cea1d6c8b6e8700727a7fa91
pandaa <vcdsxcv03@gmail.com>
2025-12-16 18:44:06 -0500

    Update version.lua

--- data/version.lua
+++ data/version.lua
@@ -1,7 +1,6 @@
 return {
-	Theme = "1.0.0",
-	SaveManager = "1.0.0",
-	UI_LIB = "1.0.0",
-	VISUAL_helper = "1.0.0",
+	{ Name = "Theme",         File = "Theme.lua",         Version = "1.0.0" },
+	{ Name = "SaveManager",   File = "SaveManager.lua",   Version = "1.0.0" },
+	{ Name = "UI_LIB",        File = "UI_LIB.lua",        Version = "1.0.0" },
+	{ Name = "VISUAL_helper", File = "VISUAL_helper.lua", Version = "1.0.0" },
 }
-
