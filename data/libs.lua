22671884c779e58bb05d5c0cafee8606aba9f815
pandaa <vcdsxcv03@gmail.com>
2025-12-17 15:53:21 -0500

    Update and rename version.lua to libs.lua

---
+++ data/libs.lua
@@ -0,0 +1,6 @@
+return {
+	{ Name = "thememanage", Version = "1.0.0" },
+	{ Name = "savemanage", Version = "1.0.0" },
+	{ Name = "library", Version = "1.0.0" },
+	{ Name = "esp", Version = "1.0.0" },
+}
--- data/version.lua
+++
@@ -1,6 +0,0 @@
-return {
-	{ Name = "thememanage.lua", Version = "1.0.0" },
-	{ Name = "savemanage.lua", Version = "1.0.0" },
-	{ Name = "library.lua", Version = "1.0.0" },
-	{ Name = "esp.lua", Version = "1.0.0" },
-}
